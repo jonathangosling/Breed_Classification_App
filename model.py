ca0ccc365d60d746aa707986e064d379336da48c
Jonathan Gosling <jonathangosling@kubrickgroup.com>
2023-06-12 18:53:33 +0100

    adhoc

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 # import tensorflow as tf
-import tensorflow_hub as hub
+import tensorflow as tf
+# from tensorflow_hub import KerasLayer
 import csv
 import numpy as np
 
 
-def load_model(model_path):
+def load_our_model(model_path):
     """
     Loads model from input path
     """
@@ -12,13 +13,13 @@ def load_model(model_path):
     # "custom_objects" used to tell Keras that there
     # is an additional (custom) layer (from training)
     model = tf.keras.models.load_model(model_path,
-                                       custom_objects={"KerasLayer": hub.KerasLayer})
+                       custom_objects={"KerasLayer": KerasLayer})
     return model
 
 
 def get_unique_labels():
     read_list = []
-    with open('unique_labels.csv', 'r') as file:
+    with open('/unique_labels.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             read_list = row
