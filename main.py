771575bb0eb3c56f0cb4dcfed094864827c59515
Jonathan Gosling <jonathangosling@kubrickgroup.com>
2023-06-12 20:32:07 +0100

    adhoc

--- main.py
+++ main.py
@@ -102,19 +102,21 @@ def get_basic_form(request: Request):
 async def get_basic_form_resp(request: Request, file: UploadFile = File(...)):
     content = await file.read()
     if file.filename[-4:] == '.png' or file.filename[-4:] == '.jpg':
-        # load the trained image classification model
-        model = load_our_model(r'/20220815-18301660588220_trained_on_80percent.h5')
-        # transform the image data to the same, batched type as trained
-        trans_data = create_data_batches([content])
-        # make the prediction
-        prediction = model.predict(trans_data)
-        # transform the prediction to a predicted species and confidence limit
-        unique_labels = get_unique_labels()
-        label = get_pred_label(prediction, unique_labels)
-        output_message = '''The model predicts that the species of the
-         animal in the image that you submitted is {} with a confindence
-         of {:2.0f}%.'''.format(label.replace("_", " ").title(),
-                                np.max(prediction)*100)
+        # # load the trained image classification model
+        # model = load_our_model(r'/20220815-18301660588220_trained_on_80percent.h5')
+        # # transform the image data to the same, batched type as trained
+        # trans_data = create_data_batches([content])
+        # # make the prediction
+        # prediction = model.predict(trans_data)
+        # # transform the prediction to a predicted species and confidence limit
+        # unique_labels = get_unique_labels()
+        # label = get_pred_label(prediction, unique_labels)
+        output_message = '''Sorry I can't process that,
+         the file must be an image of type jpg or png.'''
+        # output_message = '''The model predicts that the species of the
+        #  animal in the image that you submitted is {} with a confindence
+        #  of {:2.0f}%.'''.format(label.replace("_", " ").title(),
+        #                         np.max(prediction)*100)
     else:
         output_message = '''Sorry I can't process that,
          the file must be an image of type jpg or png.'''
